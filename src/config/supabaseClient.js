6d4a318dcfe5d1e201f71048c8f59b341fcc43a5
SujalKanani <163479456+SujalKanani@users.noreply.github.com>
2026-03-06 17:57:05 -0600

    Delete src directory

--- src/config/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseClient = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
-
-module.exports = supabaseClient;
